7e0b0cd4f7681fddad4a18561a2d038e15ffa5c6
Shrinivas Mahendra Kangralkar <88592860+Shri8055@users.noreply.github.com>
2024-12-27 13:08:29 +0530

    Add files via upload

---
+++ emp.sql
@@ -0,0 +1,7 @@
+INSERT INTO emp
+VALUES
+	(1,"John","Doe",20.50,35,"2023-01-01"),
+    (2,"Patrik","Patil",30.50,30,"2023-01-05"),
+    (3,"Peter","Patil",40.50,45,"2023-01-10");
+
+SELECT * FROM emp;
